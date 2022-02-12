59b06239023cf949cde9f69b551d6bed1d3d6b3c
Frederick <fred.chien3@gmail.com>
2022-02-12 22:48:49 +0800

    add method for displaying the results of a question

--- app/models/question.rb
+++ app/models/question.rb
@@ -14,4 +14,37 @@ class Question < ApplicationRecord
 
   has_many :responses,
     through: :answer_choices
+
+  def results
+    # results = Hash.new(0)
+    # self.answer_choices.includes(:responses).each do |ac|
+    #   results[ac.text] += 1
+    # end
+    # results
+
+    # results = Hash.new(0)
+    # acs = AnswerChoice.find_by_sql([<<-SQL, self.id]) 
+    #   SELECT answer_choices.text, COUNT(responses.id) AS num_responses
+    #   FROM answer_choices
+    #   LEFT OUTER JOIN responses ON responses.answer_choice_id = answer_choices.id
+    #   WHERE answer_choices.question_id = ?
+    #   GROUP BY answer_choices.id
+    # SQL
+    # acs.each do |ac|
+    #   results[ac.text] = ac.num_responses
+    # end
+    # results
+
+    results = Hash.new(0)
+    acs = self.answer_choices
+      .select('answer_choices.text, COUNT(responses.id) AS num_responses')
+      .left_outer_joins(:responses)
+      # .where('answer_choices.question_id = ?', self.id)
+      .group('answer_choices.id')
+    acs.each do |ac|
+      results[ac.text] = ac.num_responses
+    end
+    results
+  end
+
 end
